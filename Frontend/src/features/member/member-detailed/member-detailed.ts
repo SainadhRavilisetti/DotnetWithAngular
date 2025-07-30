c831f2e420cc1a01dc7067faa221025cbff20591
sainadhR <sainadh036@gmail.com>
2025-07-30 22:02:42 +0530

    creating front view of the member

--- Frontend/src/features/member/member-detailed/member-detailed.ts
+++ Frontend/src/features/member/member-detailed/member-detailed.ts
@@ -1,13 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { profile } from '../../../types/profile';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe,RouterLink],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css'
 })
