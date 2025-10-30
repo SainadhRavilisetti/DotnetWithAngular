cd2773dd94a16b73f5600e16119bc609c3bacdf0
Sainadh Ravilisetti <sainadh036@gmail.com>
2025-10-30 13:01:25 +0530

    new changes

---
+++ DattingApp/Migrations/20251030070143_AddUserProfileFields.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DattingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProfileFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Messages",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Messages",
+                newName: "id");
+        }
+    }
+}
